33c1812d5a165295912e1a22ea13514850272402
maxwelltram <maxwelltjrr99@gmail.com>
2021-12-22 00:59:22 -0600

    FRONTEND
    Agregamos home y about al frontend.

---
+++ Proyecto-2/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+
+
+//Componentes 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
