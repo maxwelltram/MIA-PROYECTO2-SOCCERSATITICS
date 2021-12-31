cd49287760cae4dcff28e039fed00ff02ac8a5e2
Pdante1897 <66376784+Pdante1897@users.noreply.github.com>
2021-12-31 17:21:55 -0600

    Revert "Update Index.js"
    This reverts commit 767502d2a0d99a4018135a14c66a3ad4031fefaf.

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const nodemailer = require("nodemailer");//instalar paquete npm install nodemailer
 const app = express();
 const path = require('path');
-const convertCsvToXlsx = require('@aternus/csv-to-xlsx');// npm install @aternus/csv-to-xlsx
+const convertCsvToXlsx = require('@aternus/csv-to-xlsx');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
