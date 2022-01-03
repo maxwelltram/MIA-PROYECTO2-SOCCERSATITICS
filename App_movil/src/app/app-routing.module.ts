473e161b59e654728008508e51428dc6c0b05d94
Pdante1897 <66376784+Pdante1897@users.noreply.github.com>
2022-01-03 17:03:00 -0600

    App_movil

---
+++ App_movil/src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'principal',
+    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
+  },
+  {
+    path: 'mision-vision',
+    loadChildren: () => import('./mision-vision/mision-vision.module').then( m => m.MisionVisionPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'contacto',
+    loadChildren: () => import('./contacto/contacto.module').then( m => m.ContactoPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ App_movil/src/app/mision-vision/mision-vision.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mision-vision',
+  templateUrl: './mision-vision.page.html',
+  styleUrls: ['./mision-vision.page.scss'],
+})
+export class MisionVisionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
